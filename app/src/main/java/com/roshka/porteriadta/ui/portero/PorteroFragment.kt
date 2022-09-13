0a270389641eaa95375a00b71a4b6497dd9dacc4
VikMannu <victor.ayala2a@gmail.com>
2022-09-13 15:45:30 -0400

    Update dialog_recovery.xml

--- app/src/main/java/com/roshka/porteriadta/ui/portero/PorteroFragment.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/portero/PorteroFragment.kt
@@ -1,12 +1,11 @@
 package com.roshka.porteriadta.ui.portero
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.roshka.porteriadta.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.roshka.porteriadta.databinding.FragmentLoginBinding
 
 class PorteroFragment : Fragment() {
--- app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryDialog.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryDialog.kt
@@ -1,19 +1,45 @@
 package com.roshka.porteriadta.ui.recovery
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.roshka.porteriadta.R
+import com.roshka.porteriadta.PorteroActivity
+import com.roshka.porteriadta.databinding.DialogRecoveryBinding
 
 class RecoveryDialog : BottomSheetDialogFragment() {
+    private lateinit var binding: DialogRecoveryBinding
+
+    companion object {
+        fun newInstance() = RecoveryDialog()
+    }
+
+    private lateinit var viewModel: RecoveryViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.dialog_recovery,
-            container, false)
+    ): View {
+        binding = DialogRecoveryBinding.inflate(inflater, container, false)
+
+        binding.btnRecovery.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, PorteroActivity::class.java).apply {  }
+                it.startActivity(intent)
+            }
+        }
+
+        return binding.root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[RecoveryViewModel::class.java]
+        // TODO: Use the ViewModel
     }
 }
