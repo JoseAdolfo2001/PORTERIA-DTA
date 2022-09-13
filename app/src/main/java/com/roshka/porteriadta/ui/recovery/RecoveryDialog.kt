703816f9fb821b034ade2fdde0468ccf63216c1a
VikMannu <victor.ayala2a@gmail.com>
2022-09-13 16:04:22 -0400

    Navigation error between dialog and fragment

--- app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryDialog.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryDialog.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.roshka.porteriadta.PorteroActivity
+import com.roshka.porteriadta.R
 import com.roshka.porteriadta.databinding.DialogRecoveryBinding
 
 class RecoveryDialog : BottomSheetDialogFragment() {
@@ -27,10 +30,13 @@ class RecoveryDialog : BottomSheetDialogFragment() {
         binding = DialogRecoveryBinding.inflate(inflater, container, false)
 
         binding.btnRecovery.setOnClickListener {
-            activity?.let {
-                val intent = Intent(it, PorteroActivity::class.java).apply {  }
-                it.startActivity(intent)
-            }
+            Toast.makeText(activity, "Recovery Success", Toast.LENGTH_SHORT).show()
+            it.findNavController().navigate(R.id.action_recoveryDialog_to_loginFragment)
+        }
+
+        binding.btnCancel.setOnClickListener {
+            Toast.makeText(activity, "Canceled Recovery", Toast.LENGTH_SHORT).show()
+            it.findNavController().navigate(R.id.action_recoveryDialog_to_loginFragment)
         }
 
         return binding.root
