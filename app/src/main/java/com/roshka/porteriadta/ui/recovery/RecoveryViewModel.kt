8217110bf37e393184fb7670ef31260800daf15e
VikMannu <victor.ayala2a@gmail.com>
2022-09-14 14:40:09 -0400

    Finished recovery password

--- app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryViewModel.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/recovery/RecoveryViewModel.kt
@@ -1,7 +1,29 @@
 package com.roshka.porteriadta.ui.recovery
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.roshka.porteriadta.data.RecoveryResponse
 
-class RecoveryViewModel: ViewModel() {
-    // TODO: Implement the ViewModel
+class RecoveryViewModel : ViewModel() {
+    private val _isSuccessfulRecovery = MutableLiveData<RecoveryResponse>()
+    val isSuccessfulRecovery: LiveData<RecoveryResponse>
+        get() = _isSuccessfulRecovery
+
+    fun resetPassword(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val response = RecoveryResponse(
+                    true,
+                    "Correo electrónico enviado con éxito para restablecer su contraseña"
+                )
+                _isSuccessfulRecovery.value = response
+            } else {
+                val response = RecoveryResponse(true, task.exception!!.message.toString())
+                _isSuccessfulRecovery.value = response
+            }
+        }
+    }
 }
