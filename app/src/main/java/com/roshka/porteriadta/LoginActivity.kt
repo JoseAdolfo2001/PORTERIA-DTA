808f16f959249240e9a8f285152dee647eb76788
Mathias <contactomathiasbenitez@gmail.com>
2022-09-14 10:53:39 -0300

    Login_XML_Responsive

--- app/src/main/java/com/roshka/porteriadta/LoginActivity.kt
+++ app/src/main/java/com/roshka/porteriadta/LoginActivity.kt
@@ -17,20 +17,6 @@ class LoginActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, LoginFragment.newInstance())
                 .commitNow()
-            var button = findViewById<Button>(R.id.btn)
-            var texto1 = findViewById<EditText>(R.id.et1)
-            var texto2 = findViewById<EditText>(R.id.et2)
-
-            button.setOnClickListener {
-                if(texto1.text.isNotEmpty() && texto2.text.isNotEmpty()){
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                        texto1.text.toString() , texto2.text.toString()).addOnCompleteListener {
-                        if(it.isSuccessful){
-                            Toast.makeText(this,"Hola me registre kp", Toast.LENGTH_SHORT).show()}
-                        else Toast.makeText(this,"fallo kp", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
         }
         }
     }
