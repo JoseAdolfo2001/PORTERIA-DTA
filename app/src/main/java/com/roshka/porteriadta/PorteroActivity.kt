31374390fce6f49e6b867ff44917dddf5d8680b9
VikMannu <victor.ayala2a@gmail.com>
2022-09-15 13:57:46 -0400

    Finished SplashActivity.kt

--- app/src/main/java/com/roshka/porteriadta/PorteroActivity.kt
+++ app/src/main/java/com/roshka/porteriadta/PorteroActivity.kt
@@ -2,13 +2,18 @@ package com.roshka.porteriadta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roshka.porteriadta.databinding.ActivityLoginBinding
+import com.roshka.porteriadta.databinding.ActivityPorteroBinding
 import com.roshka.porteriadta.ui.portero.PorteroFragment
 
 class PorteroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPorteroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_portero)
+
+        binding = ActivityPorteroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
---
+++ app/src/main/java/com/roshka/porteriadta/SplashActivity.kt
@@ -0,0 +1,48 @@
+package com.roshka.porteriadta
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.roshka.porteriadta.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var fb: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fb = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        if(user != null){
+            fb.collection("Users").document(user.email.toString()).get()
+                .addOnSuccessListener {
+                    val type = it.get("Nivel").toString()
+                    if(type == "admin"){
+                        val intent = Intent(this , AdminActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        val intent = Intent(this , PorteroActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+        } else {
+            val intent = Intent(this , LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
