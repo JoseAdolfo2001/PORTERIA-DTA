098830be3e1c4794bbc6788a56c73c00117caa4c
VikMannu <victor.ayala2a@gmail.com>
2022-09-19 02:26:34 -0400

    Finished menu

--- app/src/main/java/com/roshka/porteriadta/PorteroActivity.kt
+++ app/src/main/java/com/roshka/porteriadta/PorteroActivity.kt
@@ -1,9 +1,15 @@
 package com.roshka.porteriadta
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,12 +18,15 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.roshka.porteriadta.databinding.ActivityPorteroBinding
+import kotlin.math.log
 
 class PorteroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPorteroBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var auth: FirebaseAuth
+    private lateinit var fb: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,7 @@ class PorteroActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        fb = FirebaseFirestore.getInstance()
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
@@ -36,25 +46,47 @@ class PorteroActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_change_password
+                R.id.nav_home, R.id.nav_change_password, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
 
+        val headerView = binding.navView.getHeaderView(0)
+
+        val user = auth.currentUser
+
+        if (user != null) {
+            fb.collection("Users").document(user.email.toString()).get()
+                .addOnSuccessListener {
+                    val name = it.get("Nombre").toString()
+                    val nameUser = headerView.findViewById<TextView>(R.id.name_user)
+                    nameUser.text = name
+                }
+        }
+
+//        val nameUser = headerView.findViewById<TextView>(R.id.name_user)
+//        nameUser.text = user?.displayName ?: ""
+
+        val emailUser = headerView.findViewById<TextView>(R.id.email_user)
+        emailUser.text = user?.email ?: ""
+
+        val logout = binding.navView.menu.getItem(2)
+        logout.setOnMenuItemClickListener { onContextItemSelected(it) }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_portero)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    private fun showAlert(message: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage(message)
-        builder.setPositiveButton("Aceptar", null)
-        val dialog = builder.create()
-        dialog.show()
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        auth.signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        return true
     }
 }
+
+
+
