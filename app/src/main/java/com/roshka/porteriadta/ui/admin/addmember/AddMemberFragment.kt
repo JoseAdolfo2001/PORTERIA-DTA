6a0ef8ea88a7833fbc5822c08b0d52fb25e5f69e
ivan <riverosivan97@gmail.com>
2022-09-19 17:40:29 -0400

    addmember, no test bad xml

--- app/src/main/java/com/roshka/porteriadta/ui/admin/addmember/AddMemberFragment.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/admin/addmember/AddMemberFragment.kt
@@ -1,24 +1,21 @@
 package com.roshka.porteriadta.ui.admin.addmember
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.roshka.porteriadta.databinding.FragmentAddMemberBinding
+import com.roshka.porteriadta.databinding.FragmentLoginBinding
+import java.util.*
 
 class AddMemberFragment : Fragment() {
     private var _binding: FragmentAddMemberBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
     companion object {
         fun newInstance() = AddMemberFragment()
     }
-
     private lateinit var viewModel: AddMemberViewModel
 
     override fun onCreateView(
@@ -26,19 +23,63 @@ class AddMemberFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAddMemberBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        return root
-    }
-
+        val tipo = arrayOf("Socio","Gimnasio","Invitado","Guarderia")
+        val spinner = binding.spinner
+        spinner?.adapter = activity?.applicationContext?.let { ArrayAdapter(it, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, tipo) } as SpinnerAdapter
+        return binding.root
+        }
     @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this)[AddMemberViewModel::class.java]
         // TODO: Use the ViewModel
+        binding.aceptar.setOnClickListener {
+            val nombre = binding.nombre.text.toString()
+            val apellido = binding.lname.text.toString()
+            val cedula = binding.cedula.text.toString()
+            val nSocio = binding.nsocio.text.toString()
+            val tipo: String = binding.spinner.selectedItem.toString()
+            println(tipo)
+            if (checkFields(nombre, apellido, cedula, nSocio, tipo)) {
+                viewModel.setMember(apellido, nombre, cedula, nSocio, tipo)
+                jobDone()
+                clean()
+            }
+        }
     }
 
+    fun checkFields(nombre:String,apellido : String , cedula : String, nsocio:String ,tipo : String): Boolean {
+        if (nombre.isEmpty() ){
+            binding.nombre.error="Campo Requerido"
+            return false}
+        if(apellido.isEmpty()){
+            binding.lname.error="Campo Requerido"
+                    return false }
+        if(cedula.isEmpty()) {
+            binding.cedula.error="Campo Requerido"
+            return false
+        }
+        if(nsocio.isEmpty()) {
+            binding.nsocio.error = "Campo Requerido"
+            return false
+        }
+        if(tipo == "") {
+            binding.nsocio.error="Campo Requerido"
+            return false
+        }
+
+        return true
+        }
+
+    fun clean(){
+        binding.nombre.setText("")
+        binding.lname.setText("")
+        binding.cedula.setText("")
+        binding.nsocio.setText("")
+    }
+    fun jobDone(){
+        Toast.makeText(activity,"Se agrego", Toast.LENGTH_SHORT).show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/roshka/porteriadta/ui/admin/addmember/AddMemberViewModel.kt
+++ app/src/main/java/com/roshka/porteriadta/ui/admin/addmember/AddMemberViewModel.kt
@@ -1,7 +1,16 @@
 package com.roshka.porteriadta.ui.admin.addmember
 
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AddMemberViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val db = FirebaseFirestore.getInstance()
+   fun setMember(apellido : String , nombre : String , cedula : String,nsocios:String ,tipo : String){
+       db.collection("List").document(cedula).set(hashMapOf("apellido" to apellido,
+       "nombre" to nombre,"socios_numeros" to nsocios , "puede_entrar" to true , "tipo" to tipo))
+   }
 }
+
+
+
